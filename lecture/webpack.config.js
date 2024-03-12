e4a5853859a9be8f041e8283348357164b4fc4ad
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-03-12 22:16:18 +0900

    끝말잇기 리액트 웹게임 개발 환경 설정

---
+++ lecture/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path'); //node에서 제공하는 경로 조작하는 툴
+
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', //실서비스: production
+    devtool: 'eval', //빠르게
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    entry: {
+        app: ['./client'],
+    }, //입력
+    output: {
+        path: path.join(__dirname, 'dist'), //현재경로(__dirname = lecture) 안에 있는 dist 폴더
+        filename: 'app.js'
+    }, //출력
+
+};
