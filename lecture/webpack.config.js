9a95ec10244f4ef42f7d1f7b29af1fa0d314d07a
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-03-17 22:30:52 +0900

    바벨 설치 및 리액트 개발 환경 구축 완료

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -10,6 +10,16 @@ module.exports = {
     entry: {
         app: ['./client'],
     }, //입력
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+            }
+        }]
+    },
     output: {
         path: path.join(__dirname, 'dist'), //현재경로(__dirname = lecture) 안에 있는 dist 폴더
         filename: 'app.js'
--- lecture/wordRelay.jsx
+++ lecture/wordRelay.jsx
@@ -1,4 +1,14 @@
 const React = require('react');
 const { Component } = React;
 
+class WordRelay extends Component {
+    state = {
+        text: 'Hello world',
+    };
+
+    render() {
+        return <h1>{this.state.text}</h1>
+    }
+}
+
 module.exports = WordRelay;
