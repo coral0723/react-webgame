96c36abef0e116804bcff3ca8c9aa103bf63403e
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-03-23 22:17:48 +0900

    웹팩 데브 서버와 핫리로딩 추가

--- 2. 끝말잇기/webpack.config.js
+++ 2. 끝말잇기/webpack.config.js
@@ -1,28 +1,49 @@
-const path = require('path'); //node에서 제공하는 경로 조작하는 툴
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
-    name: 'wordrelay-setting',
-    mode: 'development', //실서비스: production
-    devtool: 'eval', //빠르게
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    entry: {
-        app: ['./client'],
-    }, //입력
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            }
-        }]
-    },
-    output: {
-        path: path.join(__dirname, 'dist'), //현재경로(__dirname = lecture) 안에 있는 dist 폴더
-        filename: 'app.js'
-    }, //출력
+  name: 'wordrelay',
+  mode: 'development', // 실서비스: production
+  devtool: 'eval', // 실서비스: hidden-source-map
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
 
+  entry: {
+    app: ['./client'],
+  }, // 입력
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['@babel/preset-env', {
+            targets: {
+              browsers: ['> 1% in KR'], // browserslist
+            },
+            debug: true,
+          }],
+          '@babel/preset-react',
+        ],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+          'react-refresh/babel',
+        ],
+      },
+    }],
+  },
+  plugins: [
+    new RefreshWebpackPlugin()
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    devMiddleware: { publicPath: '/dist' },
+    static: { directory: path.resolve(__dirname) },
+    hot: true
+  },
 };
