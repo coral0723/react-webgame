b9843f3ec52acae0ecaf03cec6148c7f0b4112ee
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-03-31 20:41:40 +0900

    반응속도체크 웹게임

---
+++ 4. 반응속도체크/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ResponseCheck from './ResponseCheck';
+
+// ReactDom.render(<WordRelay />, document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector('#root')).render(<ResponseCheck />);
