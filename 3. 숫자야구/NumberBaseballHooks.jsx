6b1ab1abdcea4afafbdeb42ce180a5e58014018d
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-03-31 18:31:22 +0900

    Hooks로 전환한 숫자야구 추가

---
+++ 3. 숫자야구/NumberBaseballHooks.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import TryHooks from './TryHooks';
+
+function getNumbers() { //숫자 네 개를 겹치지 않게 뽑는 함수
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for (let i = 0; i < 4; i++) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
+}
+
+const NumberBaseballHooks = () => {
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const [answer, setAnswer] = useState(getNumbers);
+    const [tries, setTries] = useState([]);
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        if (value === answer.join('')) { //홈런일때
+            setResult('홈런!');
+            setTries((prevTries) => {
+                return [...prevTries, { try: value, result: '홈런!' }]
+            });
+            alert('게임을 다시 시작합니다.');
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
+        } else { //홈런이 아닐 때
+            const answerArray = value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (tries.length >= 9) { //10번 틀렸을 때
+                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다!`);
+                alert('게임을 다시 시작합니다.');
+                setValue('');
+                setAnswer(getNumbers());
+                setTries([]);
+            } else { //10번 이하로 틀렸을 때
+                for (let i = 0; i < 4; i++) {
+                    if(answerArray[i] === answer[i]) {
+                        strike += 1;
+                    } else if (answer.includes(answerArray[i])) {
+                        ball += 1;
+                    } 
+                };
+                setTries((prevTries) => {
+                    return [...prevTries, { try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }]
+                });
+                setValue('');
+            }
+        }
+    }
+
+    const onChangeInput = (e) => {
+        setValue(e.target.value);
+    }
+
+    return (
+        <>
+            <h1>{result}</h1>
+            <form onSubmit={onSubmitForm}>
+                <input maxLength={4} value={value} onChange={onChangeInput}/>
+            </form>
+            <div>시도: {tries.length}</div>
+            <ul>
+                {tries.map((v, i) => {
+                    return (
+                        <TryHooks key={`${i + 1}차 시도`} tryInfo = {v}/>
+                    )
+                })}
+            </ul>
+        </>
+    )
+}
+
+export default NumberBaseballHooks;
