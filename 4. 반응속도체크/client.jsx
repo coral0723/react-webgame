ae4710e0cc97b29400963bf04d088bb27cf18e65
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-04-01 23:10:48 +0900

    반응속도체크 hooks 추가

--- 4. 반응속도체크/client.jsx
+++ 4. 반응속도체크/client.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ResponseCheck from './ResponseCheck';
+import ResponseCheck from './ResponseCheck-hooks';
 
 // ReactDom.render(<WordRelay />, document.querySelector('#root'));
 ReactDOM.createRoot(document.querySelector('#root')).render(<ResponseCheck />);
