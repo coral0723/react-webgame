64b825cead8f9f0bce845a604d1ca3ab1767d4bb
san9901 <san990190@gmail.com>
2024-04-02 20:56:29 +0900

    가위바위보 웹게임 class로 구현

---
+++ 5. 가위바위보/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Rsp from './Rsp';
+
+// ReactDom.render(<WordRelay />, document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector('#root')).render(<Rsp />);
