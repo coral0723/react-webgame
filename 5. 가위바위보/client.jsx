310d4d1662bc6ebdac961e12431cb7681ba5326c
san9901 <san990190@gmail.com>
2024-04-02 23:00:33 +0900

    가위바위보 웹게임 hooks 추가

--- 5. 가위바위보/client.jsx
+++ 5. 가위바위보/client.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Rsp from './Rsp';
+import Rsp from './Rsp-hooks';
 
 // ReactDom.render(<WordRelay />, document.querySelector('#root'));
 ReactDOM.createRoot(document.querySelector('#root')).render(<Rsp />);
