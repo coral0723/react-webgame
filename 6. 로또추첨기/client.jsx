7d87cb47eb31c2ed0b7e214b97d12cbe15c78f2d
san9901 <san990190@gmail.com>
2024-04-07 19:13:39 +0900

    로또추첨기 class와 hooks로 구현

---
+++ 6. 로또추첨기/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Lotto from './lotto-hooks';
+
+// ReactDom.render(<WordRelay />, document.querySelector('#root'));
+ReactDOM.createRoot(document.querySelector('#root')).render(<Lotto />);
